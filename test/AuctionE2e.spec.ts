1b4ed7f5bb5a8ac99f5abe93325a94f09f3a8494
Kyle <kyletrusler@gmail.com>
2022-02-03 09:59:38 -0800

    Add e2e spec

--- test/AuctionE2e.spec.ts
+++ test/AuctionE2e.spec.ts
@@ -1,87 +1,254 @@
-import { BigNumber, Contract, Event } from "ethers";
+import { BigNumber } from "ethers";
 
-import hre, { ethers } from "hardhat";
+import { ethers } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import {
   closeAuction,
+  CollateralData,
   createAuctionWithDefaults,
   createTokensAndMintAndApprove,
   encodeOrder,
   placeOrders,
 } from "./utilities";
+import { expect } from "chai";
+import type {
+  BondToken as BondTokenType,
+  CollateralToken as CollateralTokenType,
+  PorterAuction as PorterAuctionType,
+  EasyAuction as GnosisAuctionType,
+  BiddingToken as BiddingTokenType,
+} from "../typechain";
 
 describe("Auction", async () => {
-  let owner: SignerWithAddress;
-  let addr1: SignerWithAddress;
-  let auction: Contract;
-  let biddingToken: Contract;
-  let collateralLockerFactory: Contract;
+  // default deployer address of contracts
+  let porterSigner: SignerWithAddress;
+  // address of the example DAO which configures and runs the auction
+  let auctioneerSigner: SignerWithAddress;
+  // addresses of the bidders
+  let bidders: SignerWithAddress[];
+  let porterAuction: PorterAuctionType;
+  let gnosisAuction: GnosisAuctionType;
+  let biddingToken: BiddingTokenType;
+  let collateralToken: CollateralTokenType;
+  let bondToken: BondTokenType;
   beforeEach(async () => {
-    [owner, addr1] = await ethers.getSigners();
-    const CollateralLockerFactory = await ethers.getContractFactory(
-      "CollateralLockerFactory"
-    );
-    collateralLockerFactory = await CollateralLockerFactory.deploy();
+    [porterSigner, auctioneerSigner, ...bidders] = await ethers.getSigners();
 
-    const Auction = await ethers.getContractFactory("Auction");
-    auction = await Auction.deploy(collateralLockerFactory.address);
+    const GnosisAuction = await ethers.getContractFactory("EasyAuction");
+    gnosisAuction = (await GnosisAuction.deploy()) as GnosisAuctionType;
 
-    const BiddingToken = await ethers.getContractFactory("QaraghandyToken");
-    biddingToken = await BiddingToken.deploy(
-      "BiddingToken",
+    const PorterAuction = await ethers.getContractFactory("PorterAuction");
+    porterAuction = (await PorterAuction.deploy(
+      gnosisAuction.address
+    )) as PorterAuctionType;
+
+    const BiddingToken = await ethers.getContractFactory("BiddingToken");
+    biddingToken = (await BiddingToken.deploy(
+      "Bidding Token",
       "BT",
-      ethers.utils.parseEther("100000")
-    );
+      ethers.utils.parseEther("10000")
+    )) as BiddingTokenType;
+
+    // Mint 100 ether of tokens of collateral for auctioneerSigner
+    const CollateralToken = await ethers.getContractFactory("CollateralToken");
+    collateralToken = (await CollateralToken.connect(auctioneerSigner).deploy(
+      "Collateral Token",
+      "CT",
+      ethers.utils.parseEther("100")
+    )) as CollateralTokenType;
+
+    // The tokens minted here do not matter. The Porter Auction will mint the bondToken
+    const BondToken = await ethers.getContractFactory("BondToken");
+    bondToken = (await BondToken.connect(porterSigner).deploy(
+      "Bond Token",
+      "BT",
+      ethers.utils.parseEther("100")
+    )) as BondTokenType;
   });
-  describe("creating an auction", async () => {
-    it("creates an auction, accepts orders, and settles auction", async () => {
-      // setup
-      const { auctionId, auctioningTokenAddress } =
-        await createAuctionWithDefaults(
-          biddingToken,
-          auction,
-          collateralLockerFactory.address
-        );
-      await createTokensAndMintAndApprove(
-        auction,
-        biddingToken,
-        owner,
-        [addr1],
-        hre
-      );
-      const sellOrder = {
-        sellAmount: ethers.utils.parseEther("10"),
-        buyAmount: ethers.utils.parseEther("10"),
-        userId: BigNumber.from(1),
+  describe("Porter Auction E2E", async () => {
+    it("deposits collateral, initiates auction, settles auction", async () => {
+      /* --------------------------------------------------
+      |                                                    |
+      |                   debug info                       |
+      |                                                    |
+      -------------------------------------------------- */
+      console.log({
+        porter: porterSigner.address,
+        auctioneer: auctioneerSigner.address,
+        [`bidders(${bidders.length})`]: bidders.map((b) => b.address),
+        bondTokenAddress: bondToken.address,
+        collateralTokenAddress: collateralToken.address,
+        biddingTokenAddress: biddingToken.address,
+        porterAuctionAddress: porterAuction.address,
+        gnosisAuctionAddress: gnosisAuction.address,
+      });
+      /* --------------------------------------------------
+      |                                                    |
+      |                 set up collateral                  |
+      |                                                    |
+      ---------------------------------------------------- */
+      const collateralData: CollateralData = {
+        collateralAddress: collateralToken.address,
+        collateralValue: ethers.utils.parseEther("100"),
       };
-      const orderTx = await placeOrders(
-        addr1,
-        auction,
-        sellOrder,
+
+      // from auctioneerSigner, approve the value of collateral to the porterAuction contract
+      await collateralToken
+        .connect(auctioneerSigner)
+        .increaseAllowance(
+          porterAuction.address,
+          collateralData.collateralValue
+        );
+
+      const configureCollateralTx = await porterAuction
+        .connect(auctioneerSigner)
+        .configureCollateral(collateralData);
+
+      expect(configureCollateralTx, "Collateral deposited")
+        .to.emit(porterAuction, "CollateralDeposited")
+        .withArgs(
+          auctioneerSigner.address,
+          collateralToken.address,
+          collateralData.collateralValue
+        );
+
+      // The deposited collateral should exist in the porterAuction contract
+      expect(
+        await porterAuction.collateralInContract(
+          auctioneerSigner.address,
+          collateralToken.address
+        ),
+        "Collateral in contract"
+      ).to.be.equal(collateralData.collateralValue);
+
+      /* --------------------------------------------------
+      |                                                    |
+      |               set up GnosisAuction                 |
+      |                                                    |
+      ---------------------------------------------------- */
+      // This creates the GnosisAuction and returns the auctionId of the newly created auction
+      const { auctionId, bondTokenAddress } = await createAuctionWithDefaults(
+        auctioneerSigner,
+        biddingToken,
+        collateralData,
+        porterAuction
+      );
+
+      // After the auction is created, the auctionCount should be 1
+      expect(auctionId, "GnosisAuction counter incremented").to.be.equal(1);
+
+      // After the auction is created, the collateralInContract should be 0
+      // (or in practice, the existing value minus the collateralValue)
+      expect(
+        await porterAuction.collateralInContract(
+          auctioneerSigner.address,
+          collateralToken.address
+        ),
+        "Collateral stored in contract"
+      ).to.be.equal(0);
+
+      // The collateralInAuction should be the collateralValue (note the mapping looks up the auctionId)
+      expect(
+        await porterAuction.collateralInAuction(
+          auctionId,
+          collateralToken.address
+        ),
+        "Collateral stored in auction"
+      ).to.be.equal(collateralData.collateralValue);
+
+      /* --------------------------------------------------
+      |                                                    |
+      |                 place orders                       |
+      |                                                    |
+      ---------------------------------------------------- */
+
+      // Give tokens from porterSigner to bidders and approve for transfer to gnosis auction
+      await createTokensAndMintAndApprove(
+        gnosisAuction,
+        biddingToken,
+        porterSigner,
+        bidders
+      );
+
+      // create sell orders for all bidders addresses
+      const nrTests = bidders.length;
+      for (let i = 0; i < nrTests; i++) {
+        const sellOrder = [
+          {
+            sellAmount: ethers.utils
+              .parseEther("10")
+              .div(BigNumber.from(nrTests - 2)),
+            buyAmount: ethers.utils.parseEther("1"),
+            userId: BigNumber.from(i + 2),
+          },
+        ];
+        await placeOrders(gnosisAuction, sellOrder, auctionId);
+      }
+
+      /* ----------------------------------------------------
+      |                                                     |
+      |                     close auction                   |
+      |                                                     |
+      ---------------------------------------------------- */
+      // This increases the time to the end of the auction
+      await closeAuction(gnosisAuction, auctionId);
+
+      /* ----------------------------------------------------
+      |                                                     |
+      |              partially settle orders                |
+      |                     (for fun)                       |
+      |                                                     |
+      ---------------------------------------------------- */
+      // This settles some of the orders and moves the queue element
+      // there is an order left over (bidders.length - 1) because there needs to be
+      // at least one order left over to be able to settle the auction
+      await gnosisAuction.precalculateSellAmountSum(
         auctionId,
-        hre
+        bidders.length - 1
       );
-      const orderRecipt = await orderTx.wait();
-      const sellAmount = orderRecipt.events.find(
-        (e: Event) => e.event === "NewSellOrder"
-      ).args.sellAmount;
 
-      await closeAuction(auction, auctionId);
-      const tx = await auction.settleAuction(auctionId);
-      const gasUsed = (await tx.wait()).gasUsed;
+      /* ----------------------------------------------------
+      |                                                     |
+      |                 settle auction                      |
+      |                                                     |
+      ---------------------------------------------------- */
+      const settleTx = await gnosisAuction.settleAuction(auctionId);
+      expect(settleTx).to.emit(gnosisAuction, "AuctionCleared");
 
-      const claimTx = await auction.claimFromParticipantOrder(auctionId, [
-        encodeOrder(sellOrder),
-      ]);
-      const claimRecipt = await claimTx.wait();
-      console.log(claimRecipt);
-      console.log("Gas usage for verification", gasUsed.toString());
-      console.log(
-        await biddingToken
-          .attach(auctioningTokenAddress)
-          .balanceOf(addr1.address)
-      );
+      /* --------------------------------------------------
+      |                                                    |
+      |                 check results                      |
+      |                                                    |
+      ---------------------------------------------------- */
+      // TODO: not sure what to expect here, probably something with getting the settlement amount
+      // and checking that each bidder has the correct amount of tokens for now
+      // confirm that there is a change in bondTokens
+
+      // before claiming from all orders, the bidding token should be 0
+      expect(
+        await bondToken.attach(bondTokenAddress).balanceOf(bidders[0].address)
+      ).to.be.eq(0);
+
+      for (let i = 0; i < nrTests; i++) {
+        const sellOrder = {
+          sellAmount: ethers.utils
+            .parseEther("10")
+            .div(BigNumber.from(nrTests - 2)),
+          buyAmount: ethers.utils.parseEther("1"),
+          userId: BigNumber.from(i + 2),
+        };
+        const claimTx = await gnosisAuction.claimFromParticipantOrder(
+          auctionId,
+          [encodeOrder(sellOrder)]
+        );
+        expect(claimTx).to.emit(gnosisAuction, "ClaimedFromOrder");
+      }
+
+      // after, the bond token should be assigned to the account
+      expect(
+        await bondToken.attach(bondTokenAddress).balanceOf(bidders[0].address)
+      ).to.be.gt(0);
     });
   });
 });
