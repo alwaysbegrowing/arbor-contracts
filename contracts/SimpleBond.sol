9809fe7fc280bb615064b19b379a66eb1888753f
luckyrobot <97241616+luckyrobot@users.noreply.github.com>
2022-02-08 10:05:40 -0600

    (fix) bond review :tada: (#20)
    * review fixes :tada:
    
    * add nonReentrant
    
    * BondStatusChange
    
    * pr fixes
    
    * add more tests !
    
    * organize the tests
    
    * use the bond standing enum

--- contracts/SimpleBond.sol
+++ contracts/SimpleBond.sol
@@ -1,17 +1,9 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-import "hardhat/console.sol";
-
-contract SimpleBond is ERC20Burnable, Ownable {
-  event Redeem(address receiver, uint256 amount);
-  event Deposit(address sender, uint256 amount);
-
-  /// @notice this date is when the DAO must have repaid its debt
-  /// @notice when bondholders can redeem their bonds
-  uint256 public maturityDate;
-
+contract SimpleBond is ERC20Burnable, Ownable, ReentrancyGuard {
   /// @notice this would go into default if maturityDate passes and the loan contract has not been paid back
   /// @notice to be set from the auction
   enum BondStanding {
@@ -21,7 +13,14 @@ contract SimpleBond is ERC20Burnable, Ownable {
     REDEEMED
   }
 
-  /// @notice holds address to bond standings
+  event BondStandingChange(BondStanding oldStanding, BondStanding newStanding);
+  event Redeem(address receiver, uint256 amount);
+
+  /// @notice this date is when the DAO must have repaid its debt
+  /// @notice when bondholders can redeem their bonds
+  uint256 public immutable maturityDate;
+
+  /// @notice holds address to bond standing
   BondStanding public currentBondStanding;
 
   /// @dev New bond contract will be deployed before each auction
@@ -36,46 +35,42 @@ contract SimpleBond is ERC20Burnable, Ownable {
     uint256 _maturityDate
   ) ERC20(_name, _symbol) {
     require(_totalBondSupply > 0, "zeroMintAmount");
+
+    // this timestamp is a date in 2020, which basically is here to confirm
+    // the date provided is greater than 0 and a valid timestamp
     require(_maturityDate > 1580752251, "invalid date");
 
     // This mints bonds based on the config given in the auction contract and
     // sends them to the auction contract
     _mint(msg.sender, _totalBondSupply);
     maturityDate = _maturityDate;
-    setBondStanding(BondStanding.GOOD);
-
-    console.log(
-      "Created tokenized bonds with totalSupply of",
-      _totalBondSupply
-    );
+    currentBondStanding = BondStanding.GOOD;
   }
 
   /// @notice To be set after the auction ends
-  function setBondStanding(BondStanding standing) public onlyOwner {
-    currentBondStanding = standing;
+  function setBondStanding(BondStanding newStanding) external onlyOwner {
+    emit BondStandingChange(currentBondStanding, newStanding);
+
+    currentBondStanding = newStanding;
   }
 
-  function redeemBond(uint256 numberOfBonds) public {
-    require(totalSupply() > 0, "invalid total supply");
-    require(
-      numberOfBonds > 0 || numberOfBonds > totalSupply(),
-      "invalid numberOfBonds"
-    );
+  function redeemBond(uint256 bondShares) external nonReentrant {
+    require(bondShares > 0, "invalid amount");
     require(block.timestamp >= maturityDate, "bond still immature");
 
     // check that the DAO has already paid back the bond, set from auction
     require(currentBondStanding == BondStanding.PAID, "bond not yet paid");
 
-    burn(numberOfBonds);
+    burn(bondShares);
 
     // TODO: code needs added here that sends the investor their how much they are owed in paymentToken
-    // this might be calling the auction contract with AuctionContract.redeem(msg.sender, numberOfBonds * faceValue)
+    // this might be calling the auction contract with AuctionContract.redeem(msg.sender, bondShares * faceValue)
 
     // once all bonds are burned, then this can be set to redeemed
-    if (totalSupply() <= 0) {
-      setBondStanding(BondStanding.REDEEMED);
+    if (totalSupply() == 0) {
+      currentBondStanding = BondStanding.REDEEMED;
     }
 
-    emit Redeem(msg.sender, numberOfBonds);
+    emit Redeem(msg.sender, bondShares);
   }
 }
